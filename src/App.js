624c172128866c6177bf886682b4ab32164aecda
praiseokelade <151168193+praiseokelade@users.noreply.github.com>
2024-05-16 14:06:20 +0100

    Added GitHub URL

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import Weather from "./Weather";
-import React from "react"
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <h1>Weather Search Engine</h1>
       <Weather />
+      <a href="https://github.com/praiseokelade/weather-react-apphttps://github.com/praiseokelade/weather-react-app">
+        Open-sourced on GitHub
+      </a>
     </div>
   );
 }
